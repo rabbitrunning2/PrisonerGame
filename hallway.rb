aac6933b5abd0b2ca1fd738592f0cb5c3cd93e81
Ryan Esber <ichabod12001@yahoo.com>
2013-12-18 20:56:46 -0800

    Cleaned up  hallway.rb

--- hallway.rb
+++ hallway.rb
@@ -1,4 +1,5 @@
 require_relative 'room.rb'
+require_relative 'death.rb'
 	
 class Hallway < Room
 	
@@ -12,7 +13,7 @@ class Hallway < Room
 		@room_130_description = "You open the door and walk into the cell. 
 		There is a man's body lying on the floor. He looks dead .There is dried blood and bruises all over his body.
 		His clothes are torn. There is a piece of paper by his hand."
-		display_description(description)
+		display_description(hallway_description)
 		start()
 	end
 	
@@ -30,41 +31,46 @@ DESCRIPTION
 			prompt()
 			action = gets.chomp
 			
-		if string_check(action, 'help')
-			help_request(@hallway_help)
-		elsif string_check(action, 'look') or string_check(action, 'search')
-			puts "There are five doors numbered: 130, 131, 133, 134, 135, 
-				two larger doors to your right and to your left."
-		elsif string_check(action, 'open') and string_check(action, 'door')
-			door_choice()
+			if string_check(action, 'help')
+				help_request(@hallway_help)
+			elsif string_check(action, 'look') or string_check(action, 'search')
+				puts "There are five doors numbered: 130, 131, 133, 134, 135," 
+				puts "two larger doors to your right and to your left."
+			elsif string_check(action, 'open') and string_check(action, 'door')
+				door_choice
 		
-		else
-			puts "You can't do that."
+			else
+				puts "You can't do that."
+			end
 		end
 	
 	end
 	
 	def door_choice
 		puts "Which door do you want to open?"
-		if string_check(action, 'help')
-			help_request(@hallway_help)
-		elsif string_check(action, 'right')
-			right_end_door
-		elsif string_check(action, 'left')
-			left_end_door
-		elsif string_check(action, '130')
-			room_130
-		elsif string_check(action, '131') or string_check(action, '133') or string_check(action, '135')
-			locked_door
-		elsif string_check(action, '134')
-			room_134
-		elsif string_check(action, '132')
-			puts "Yeah, why bother trying to escape."
-			puts "You go back to your cell and lie down."
-			puts "You don't wait long before a shadowy figure comes in and puts a bullet in your head."
-			Death.new
-		else 
-			puts "You can't do that."
+		while true
+			prompt()
+			action = gets.chomp
+			if string_check(action, 'help')
+				help_request(@hallway_help)
+			elsif string_check(action, 'right')
+				right_end_door
+			elsif string_check(action, 'left')
+				left_end_door
+			elsif string_check(action, '130')
+				room_130
+			elsif string_check(action, '131') or string_check(action, '133') or string_check(action, '135')
+				locked_door
+			elsif string_check(action, '134')
+				room_134
+			elsif string_check(action, '132')
+				puts "Yeah, why bother trying to escape."
+				puts "You go back to your cell and lie down."
+				puts "You don't wait long before a shadowy figure comes in and puts a bullet in your head."
+				Death.new
+			else 
+				puts "You can't do that."
+			end
 		end
 	end
 	
@@ -81,6 +87,7 @@ DESCRIPTION
 			if string_check(action, 'help')
 				help_request(@room_134_help)
 			elsif string_check(action, 'leave') or string_check(action, 'exit')
+				puts "You are back in the hallway."
 				start()
 			elsif string_check(action, 'look') or string_check(action, 'search')
 				puts "You look around, but find nothing of use."
@@ -98,12 +105,13 @@ DESCRIPTION
 			if string_check(action, 'help')
 				help_request(@room_130_help)
 			elsif string_check(action, 'paper')
-				puts "It appears to be a confession of sorts. 
-				It says that he is a traitor and sold state secrets.
-				The heading on the paper says Department of Defense and has an emblem with an eagle on it."		
+				puts "It appears to be a confession of sorts." 
+				puts "It says that he is a traitor and sold state secrets."
+				puts "The heading on the paper says Department of Defense and has an emblem with an eagle on it."		
 			elsif string_check(action, 'body')
 				puts "You search the man. He appears to have been beaten to death. He has nothing on him."
 			elsif string_check(action, 'hallway') or string_check(action, 'leave')
+				puts "You are back in the hallway."
 				start()
 			else 
 				puts "You can't do that."
