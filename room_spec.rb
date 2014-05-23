5341711e4eb35a2d4bfb7fce36c883544046c1c5
Ryan Esber <ichabod12001@yahoo.com>
2014-05-22 21:16:20 -0500

    Added rspec tests for string_check method in Room class.

--- room_spec.rb
+++ room_spec.rb
@@ -5,4 +5,15 @@ describe Room do
 	it "should initialize" do	
 		room = Room.new
 	end
+	
+	it "should return true for word found in string" do
+		room = Room.new
+		room.string_check("I am walking", "am")
+	end
+	
+		it "should return false for word not found in string" do
+		room = Room.new
+		room.string_check("I am walking", "tree")
+	end
+	
 end
