67d1821bfc3da9227a78c3d6a2ae64d93ec037a4
Ryan <ichabod12001@yahoo.com>
2014-05-28 11:17:36 -0500

    added methods and descriptions to hallway_four.rb and hallway_three.rb

--- hallway_four.rb
+++ hallway_four.rb
@@ -1,3 +1,48 @@
 require_relative 'room.rb'
 class Hallway_Four < Room
+def initialize()
+		display_description(hallway_description)
+		start()
+	end
+	
+	def hallway_description
+		<<DESCRIPTION
+You walk out into a corridor.
+There are six doors in the hallway numbered: 111, 112, 113, 111, 15, 116.
+There are two larger doors at either end of the hallway.
+DESCRIPTION
+	end
+	def room_111_description
+	end
+	def room_112_description
+	end
+	def room_113_description
+	end
+	def room_111_description
+	end
+	def room_115_description
+	end
+	def room_116_description
+	end
+	
+	def start()
+	end
+	
+	def room_111
+	end
+	
+	def room_112
+	end
+	
+	def room_113
+	end
+	
+	def room_111
+	end
+	
+	def room_115
+	end
+	
+	def room_116
+	end
 end
--- hallway_three.rb
+++ hallway_three.rb
@@ -1,3 +1,48 @@
 require_relative 'room.rb'
 class Hallway_Three < Room
+def initialize()
+		display_description(hallway_description)
+		start()
+	end
+	
+	def hallway_description
+		<<DESCRIPTION
+You walk out into a corridor.
+There are six doors in the hallway numbered: 121, 122, 123, 122, 15, 126.
+There are two larger doors at either end of the hallway.
+DESCRIPTION
+	end
+	def room_121_description
+	end
+	def room_122_description
+	end
+	def room_123_description
+	end
+	def room_122_description
+	end
+	def room_125_description
+	end
+	def room_126_description
+	end
+	
+	def start()
+	end
+	
+	def room_121
+	end
+	
+	def room_122
+	end
+	
+	def room_123
+	end
+	
+	def room_122
+	end
+	
+	def room_125
+	end
+	
+	def room_126
+	end
 end
